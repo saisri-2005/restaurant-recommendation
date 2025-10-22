db0a3555614ca30c4836f7d0951ab9621bb0048d
Kosaraju Sai Sri <ksaisri13@gmail.com>
2025-10-23 00:36:35 +0530

    Updated App.js and MapComponent.js with expanded restaurant list and improved UI

--- backend/models/restaurants.js
+++ backend/models/restaurants.js
@@ -1,23 +1,82 @@
 [
-  {"_id":"1","name":"Tandoori Delight","cuisine":["Indian","North Indian"],"location":"Hyderabad","rating":4.5,"lat":17.385044,"lng":78.486671},
-  {"_id":"2","name":"Sushi World","cuisine":["Japanese","Seafood"],"location":"Bangalore","rating":4.2,"lat":12.971599,"lng":77.594566},
-  {"_id":"3","name":"Pizza Planet","cuisine":["Italian","Fast Food"],"location":"Mumbai","rating":3.8,"lat":19.07609,"lng":72.877426},
-  {"_id":"4","name":"Burger Hub","cuisine":["American","Fast Food"],"location":"Delhi","rating":4.0,"lat":28.613939,"lng":77.209023},
-  {"_id":"5","name":"Curry Corner","cuisine":["Indian","South Indian"],"location":"Chennai","rating":4.3,"lat":13.08268,"lng":80.270718},
-  {"_id":"6","name":"Noodle House","cuisine":["Chinese","Asian"],"location":"Kolkata","rating":4.1,"lat":22.572646,"lng":88.363895},
-  {"_id":"7","name":"Pasta Fresca","cuisine":["Italian"],"location":"Pune","rating":4.0,"lat":18.520430,"lng":73.856743},
-  {"_id":"8","name":"Spice Route","cuisine":["Indian","Chinese"],"location":"Ahmedabad","rating":4.4,"lat":23.022505,"lng":72.571362},
-  {"_id":"9","name":"Seafood Shack","cuisine":["Seafood"],"location":"Goa","rating":4.3,"lat":15.299326,"lng":74.123993},
-  {"_id":"10","name":"Bagel Bros","cuisine":["American","Fast Food"],"location":"Jaipur","rating":3.9,"lat":26.912434,"lng":75.787270},
-  {"_id":"11","name":"Vegan Delight","cuisine":["Vegan","Healthy"],"location":"Bhubaneswar","rating":4.2,"lat":20.296059,"lng":85.824539},
-  {"_id":"12","name":"Kebab Kingdom","cuisine":["Indian","Mughlai"],"location":"Lucknow","rating":4.5,"lat":26.846695,"lng":80.946167},
-  {"_id":"13","name":"Dim Sum House","cuisine":["Chinese"],"location":"Chandigarh","rating":4.0,"lat":30.733315,"lng":76.779419},
-  {"_id":"14","name":"Burger Fiesta","cuisine":["American","Fast Food"],"location":"Kochi","rating":3.8,"lat":9.931233,"lng":76.267304},
-  {"_id":"15","name":"Mediterraneo","cuisine":["Mediterranean","Italian"],"location":"Goa","rating":4.3,"lat":15.299326,"lng":74.123993},
-  {"_id":"16","name":"Biryani Palace","cuisine":["Indian","Hyderabadi"],"location":"Hyderabad","rating":4.7,"lat":17.395044,"lng":78.496671},
-  {"_id":"17","name":"Dragon Wok","cuisine":["Chinese"],"location":"Bangalore","rating":4.1,"lat":12.981599,"lng":77.604566},
-  {"_id":"18","name":"Taco Town","cuisine":["Mexican"],"location":"Mumbai","rating":4.0,"lat":19.08609,"lng":72.887426},
-  {"_id":"19","name":"BBQ Nation","cuisine":["Indian","Grill"],"location":"Delhi","rating":4.4,"lat":28.623939,"lng":77.219023},
-  {"_id":"20","name":"Idli Street","cuisine":["South Indian"],"location":"Chennai","rating":4.3,"lat":13.09268,"lng":80.280718}
-  // ... (you can continue duplicating with variations up to 100)
+  {
+    "name": "The Spice Route",
+    "cuisine": ["Indian", "Thai"],
+    "location": "Delhi",
+    "rating": 4.6,
+    "lat": 28.6139,
+    "lng": 77.209
+  },
+  {
+    "name": "Truffles",
+    "cuisine": ["Cafe", "Fast Food"],
+    "location": "Bangalore",
+    "rating": 4.4,
+    "lat": 12.9716,
+    "lng": 77.5946
+  },
+  {
+    "name": "Biryani House",
+    "cuisine": ["Indian", "Hyderabadi"],
+    "location": "Hyderabad",
+    "rating": 4.3,
+    "lat": 17.385,
+    "lng": 78.4867
+  },
+  {
+    "name": "Barbeque Nation",
+    "cuisine": ["Indian", "Buffet"],
+    "location": "Chennai",
+    "rating": 4.2,
+    "lat": 13.0827,
+    "lng": 80.2707
+  },
+  {
+    "name": "Leopold Cafe",
+    "cuisine": ["Continental", "Bar"],
+    "location": "Mumbai",
+    "rating": 4.5,
+    "lat": 18.9218,
+    "lng": 72.8335
+  },
+  {
+    "name": "The Coffee Cup",
+    "cuisine": ["Cafe", "Bakery"],
+    "location": "Secunderabad",
+    "rating": 4.3,
+    "lat": 17.4399,
+    "lng": 78.4983
+  },
+  {
+    "name": "Annapurna Bhavan",
+    "cuisine": ["South Indian", "Vegetarian"],
+    "location": "Coimbatore",
+    "rating": 4.4,
+    "lat": 11.0168,
+    "lng": 76.9558
+  },
+  {
+    "name": "Mainland China",
+    "cuisine": ["Chinese", "Asian"],
+    "location": "Kolkata",
+    "rating": 4.3,
+    "lat": 22.5726,
+    "lng": 88.3639
+  },
+  {
+    "name": "Olive Bistro",
+    "cuisine": ["Mediterranean", "Italian"],
+    "location": "Hyderabad",
+    "rating": 4.6,
+    "lat": 17.4375,
+    "lng": 78.3957
+  },
+  {
+    "name": "The French Loaf",
+    "cuisine": ["Bakery", "Cafe"],
+    "location": "Chennai",
+    "rating": 4.1,
+    "lat": 13.035,
+    "lng": 80.244
+  }
 ]
